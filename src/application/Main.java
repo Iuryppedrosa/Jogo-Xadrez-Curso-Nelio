09957c86c3365f13cbe5c31d1025a1a1d48e3e28
iurypedrosa <iuryppedrosa@protonmail.com>
2023-07-27 15:41:02 -0300

    Implementacao Board e Piece classes

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,12 @@
 package application;
 
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
-        Position p = new Position(3, 5);
-        System.out.println(p);
+        Board b = new Board(8, 8);
+
     }
 }
