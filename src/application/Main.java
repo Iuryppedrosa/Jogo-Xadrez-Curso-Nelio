6a2615d40b366f579d3686e935ae0590136f5f2c
iurypedrosa <iuryppedrosa@protonmail.com>
2023-08-01 16:15:50 -0300

    movendo peças.

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,11 +4,30 @@ package application;
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
 
-        UI.printBoard(chessMatch.getPieces());
+        Scanner input = new Scanner(System.in);
+
+        while(true){
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Sorce: ");
+            ChessPosition source = UI.readChessPosition(input);
+
+            System.out.println();
+            System.out.print("Targe position: ");
+            ChessPosition target = UI.readChessPosition(input);
+
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+
     }
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -44,6 +44,22 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
+        if(piece(position) == null){
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+
+        pieces[position.getRow()][position.getColumn()] = null;
+
+        return aux;
+
+    }
+
     private boolean positionExists(int row, int colunm){
         return row >= 0 && row < rows && colunm >= 0 && colunm < colunms;
     }
@@ -58,5 +74,7 @@ public class Board {
         }
         return piece(position) != null;
     }
+
+
 
 }
