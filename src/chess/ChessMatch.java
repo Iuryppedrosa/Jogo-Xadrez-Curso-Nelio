2837370e9a14257cf062ebff6da8f6bb364dd684
iurypedrosa <iuryppedrosa@protonmail.com>
2023-08-29 15:38:24 -0300

    possiveis movimentos para uma peça.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -49,6 +49,9 @@ public class ChessMatch
         if (!board.thereIsAPiece(position)){
             throw new ChessException("Nao existe peca nessa posicao");
         }
+        if(!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("Náo tem movimentos possíveis para a peça escolhida");
+        }
     }
 
 
